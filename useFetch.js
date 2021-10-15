531d5e4da818cc38b4111468733a308111f30615
Breiner Barraza <breinerbarraza22@gmail.com>
2021-10-15 15:05:17 -0500

    primer commit

---
+++ useFetch.js
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+export const useFecth = ( url ) => {
+
+    const [state, setState] = useState({ data: null, loading:true, error:null })
+    const isMounted = useRef(true)
+
+    useEffect(() => {
+        return  () =>{
+            isMounted.current = false;
+        }
+    }, [])
+
+    useEffect(()=>{
+        setState({data:null, loading:true, error:null});
+
+        fetch(url)
+            .then(resp =>resp.json())
+            .then(data =>{
+                    if(isMounted.current){
+
+                           setState({
+                        loading:false,
+                        error:false,
+                        data        
+                     })
+                    }
+                
+        })
+    },[url])
+    return state;
+}
